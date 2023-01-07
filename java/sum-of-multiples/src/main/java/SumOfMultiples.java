9b1c3dc8e585b10c49b33c4bcec153620bf0a2ba
alemoles <alemolerio@gmail.com>
2023-01-06 23:36:04 -0300

    Sum of Multiples

--- java/sum-of-multiples/src/main/java/SumOfMultiples.java
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -1,4 +1,9 @@
+import static java.util.Arrays.stream;
+import static java.util.stream.IntStream.range;
+
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 class SumOfMultiples {
 
@@ -6,20 +11,27 @@ class SumOfMultiples {
     private final int number;
 
     SumOfMultiples(int number, int[] set) {
-        this.set = set;
+        this.set = stream(set).filter(value -> value != 0)
+          .toArray();
         this.number = number;
     }
 
     int getSum() {
-
-        return Arrays.stream(set)
-          .filter(value -> value != 0)
-          .map(operand -> number / operand)
-          .distinct()
-          .map(operand -> operand)
-          .sum();
+        Set<Integer> factors = new HashSet<>();
+        for (int i = 1; i < number; i++) {
+            for (int factor : set) {
+                if (i % factor == 0) {
+                    factors.add(i);
+                }
+            }
+        }
+        return factors.stream()
+          .reduce(0, Integer::sum);
     }
 
-
-
+    int getSumVariant() {
+        return range(0, number)
+          .filter(number -> stream(set).anyMatch(factor -> factor > 0 && number % factor == 0))
+          .sum();
+    }
 }
